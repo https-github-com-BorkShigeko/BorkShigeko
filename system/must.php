b29c71b35400d67a8f621e60f66c94fe8c221b9d
Sakurako1210 <s1250132@u-aizu.ac.jp>
2019-12-18 17:05:13 +0900

    1218

--- system/must.php
+++ system/must.php
@@ -3,53 +3,53 @@
 function getHeader($mode)
 {
     $headerStatus = [
-        'top'=>
+        'top' =>
             [
-                'title'=>'TOP',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/slick.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'TOP',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/slick.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
-        'about'=>
+        'about' =>
             [
-                'title'=>'ABOUT',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/about.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'ABOUT',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/about.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
-        'book'=>
+        'book' =>
             [
-                'title'=>'BOOK',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/book.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'BOOK',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/book.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
-        'event'=>
+        'event' =>
             [
-                'title'=>'EVENT',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/event.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'EVENT',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/event.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
-        'press'=>
+        'press' =>
             [
-                'title'=>'PRESS',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/press.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'PRESS',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/press.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
-        'blog'=>
+        'blog' =>
             [
-                'title'=>'BLOG',
-                'keywords'=>'A,B,C',
-                'description'=>'詳細が入ります。',
-                'css'=>'<link rel="stylesheet" href="/css/blog.css">',
-                'noindex'=>'<meta name="robots" content="noindex">'
+                'title' => 'BLOG',
+                'keywords' => 'A,B,C',
+                'description' => '詳細が入ります。',
+                'css' => '<link rel="stylesheet" href="/css/blog.css">',
+                'noindex' => '<meta name="robots" content="noindex">'
             ],
     ];
     echo '
@@ -59,11 +59,11 @@ function getHeader($mode)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
-   '.$headerStatus[$mode]['noindex'].'
+   ' . $headerStatus[$mode]['noindex'] . '
         <!--<link rel="shortcut icon" href="/img/favicon.ico">-->
-    <title>'.$headerStatus[$mode]['title'].'</title>
-    <meta name="keywords" content=" '.$headerStatus[$mode]['keywords'].'">
-    <meta name="description" content=" '.$headerStatus[$mode]['description'].'">
+    <title>' . $headerStatus[$mode]['title'] . '</title>
+    <meta name="keywords" content=" ' . $headerStatus[$mode]['keywords'] . '">
+    <meta name="description" content=" ' . $headerStatus[$mode]['description'] . '">
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/slick.css">
     <link rel="stylesheet" href="/css/slick-theme.css">
@@ -71,7 +71,7 @@ function getHeader($mode)
     <link rel="stylesheet" href="/css/my.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/myAnimation.css">
-     '.$headerStatus[$mode]['css'].'
+     ' . $headerStatus[$mode]['css'] . '
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
           integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
@@ -100,20 +100,42 @@ function getHeader($mode)
         </header>
         <nav class="myGlobalNav myBack">
             <div class="container mySection myBack">
-                <div class="mySectionInner">
+                <div class="mySectionInner" id="menu">
+                <div class="menuSideBar">
+                    <img src="/img/menu_bar.png">
+                </div>
                     <div class="mySectionTitle">
-                        <h2 class="en">MENU</h2>
-                        <p class="mySubTitle">メニュー</p>
+                      <h2>MENU</h2>
                     </div>
                     <ul class="en">
-                        <li><a href="/"><p>TOP</p></a></li>
-                        <li><a href="/about"><p>ABOUT</p></a></li>
-                        <li><a href="index.php"><p>BOOK</p></a></li>
-                        <li><a href="index.php"><p>EVENT</p></a></li>
-                        <li><a href="member.php"><p>MEMBER PAGE</p></a></li>
-                        <li><a href="index.php"><p>PRESS</p></a></li>
-                        <li><a href="index.php"><p>BLOG</p></a></li>
+                        <li><a href="/"><p class="english">TOP</p>
+                        <p class="japanese">トップ</p></a></li>
+                        
+                        <li><a href="/about"><p class="english">ABOUT</p>
+                        <p class="japanese">自己紹介</p></a></li>
+                        
+                        <li><a href="/book"><p class="english">BOOK</p>
+                        <p class="japanese">著書</p></a></li>
+                        
+                        <li><a href="/event"><p class="english">EVENT</p>
+                        <p class="japanese">イベント・講演会</p></a></li>
+                        
+                        <li><a href="..."><p class="english">MEMBER PAGE</p>
+                        <p class="japanese">有料会員ページ</p></a></li>
+                        
+                        <li><a href="/#press"><p class="english">PRESS</p>
+                        <p class="japanese">プレス</p></a></li>
+                        <li><a href="/blog"><p class="english">BLOG</p>
+                        <p class="japanese">ブログ</p></a></li>
                     </ul>
+                    
+                    <div class="menuIcon">
+                    <a href="..."><img src="/img/menu_facebook.png"></a>
+                    <a href="..."><img src="/img/menu_instagram.png"></a>
+                    <a href="..."><img src="/img/menu_twitter.png"></a>
+                    <a href="..."><img src="/img/menu_youtube.png"></a>
+                    </div>
+                    <div class="copyright"><p>&copyCOPYRIGHT XXXXXXXXXX</p></div>
                 </div>
             </div>
         </nav>
