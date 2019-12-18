b075270cf0eb6b14dec133f0e0bf106494f84a80
Sakurako1210 <s1250132@u-aizu.ac.jp>
2019-12-18 14:37:07 +0900

    1218

--- index.php
+++ index.php
@@ -8,24 +8,25 @@ getHeader('top');
         <div class="mySectionInner">
 
             <div class="aboveMv">
-                <h1>あああああああああああああ</h1>
+                <h1>自分史上最高に幸せになる!</h1>
             </div>
             <div class="mainVisual">
-                <div class="mvBack"></div>
                 <div class="mvPic">
                     <img class="spShigeko" src="img/top_shigeko.png" alt="Shigeko pic(sp)">
                     <img class="pcShigeko" src="img/top_shigeko_pc.png" alt="Shigeko pic(pc)">
                 </div>
             </div>
             <div class="BorkShigeko">
+                <p><span>B</span>ork <span>S</span>higeko</p>
                 <h2>ボーク重子</h2>
-                <h3>あああああああ<br>
-                    +<br>
-                    あああああああ<br>
-                    +<br>
-                    あああああああ<br>
-                    =<br>
-                    あああああああ
+                <h3 class="job">ICF会員ライフコーチ / 作家 /<br>
+                    スピーカー / アートコンサルタント</h3>
+                <h3 class="BorkShigekoKeyword">
+                    <span>アート業界・起業</span>
+                    <span>「ワシントンの美しい25人」</span>
+                    <span>ベストセラー作家</span>
+                    <span> 全米最優秀女子高生」の母</span>
+                    <span> 非認知能力</span>
                 </h3>
             </div>
         </div>
@@ -34,141 +35,229 @@ getHeader('top');
     <div class="mySection" id="why">
         <div class="mySectionInner">
             <div class="mySectionBack"></div>
-            <div class="shigekoLogo">
-                <img src="/img/shigeko_font.png" alt="shigeko Bork">
-            </div>
+
             <div class="whyShigeko">
-                <img src="img/why_back.png" alt="fukidashi">
-            </div>
-            <div class="whyLetter">
-                <p>
-                    ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="happyLife">
-                <h2>自分史上最高に幸せになる!<br>
-                    ああああああああああああ</h2>
-                <img src="/img/line.png" alt="line">
-            </div>
-            <div class="messageMoveBack"></div>
-            <div class="messageMovie"></div>
-            <p class="messageMovieP">Message Movie</p>
-        </div>
-    </div>
-
-    <div class="mySection" id="registration">
-        <div class="mySectionInner">
-            <h2 class="registrationTitle">会員登録(有料)のご案内</h2>
-            <div class="content1back"></div>
-
-            <div class="content1">
-                <dl>
-                    <dt>
-                        <ruby>
-                            <rb>Message</rb>
-                            <rp>(</rp>
-                            <rt>メッセージ</rt>
-                            <rp>)</rp>
-                        </ruby>
-                    </dt>
-                    <dd>
-                        ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-                    </dd>
-                </dl>
-            </div>
-
-            <img class="signature" src="...">
-
-            <div class="content2back"></div>
-            <div class="content2">
-                <dl>
-                    <dt>
-                        <ruby>
-                            <rb>Special</rb>
-                            <rp>(</rp>
-                            <rt>スペシャル</rt>
-                            <rp>)</rp>
-                        </ruby>
-                    </dt>
-                    <dd>
-                        ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-                    </dd>
-                </dl>
-            </div>
-
-            <div class="content3">
-                <dl>
-                    <dt>
-                        <ruby>
-                            <rb>Price</rb>
-                            <rp>(</rp>
-                            <rt>会費</rt>
-                            <rp>)</rp>
-                        </ruby>
-                    </dt>
-                    <dd>
-                        ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
-                    </dd>
-                </dl>
-            </div>
-            <div class="registrationFoot">
-                <div class="terms">
-                    <p>規約
-                        <span>Terms of Service</span></p>
+                <img class="fukidashi" src="/img/why_back.png">
+                <div class="inFukidashi">
+                    <h3><span>W</span>hy <span>S</span>higeko?</h3>
+                    <p>どうしてボーク重子なのか？</p>
                 </div>
-                <div class="register">
-                    <p>加入する
-                        <span>Registration</span></p>
+                <div class="whyLetter">
+                    <p>
+                        「世界最高の子育て（ダイヤモンド社2018年出版）」がベストセラーとなって以来、「非認知能力」の火付け役として講演会や執筆のオファーが殺到。その後出版された本は６冊、アジア圏での出版は３冊。講演会とワークショップの参加者は6500名を超える。「人生が変わる1分間の深イイ話」をはじめメディアの登場は50媒体以上（2019年12月時点）。<br>
+                        <br>
+                        「パッション」が代名詞ともなっているライフコーチ・ボーク重子の声に多くの共感が寄せられるのはなぜ？<br>
+                        <br>
+                        それは「等身大」だから。特別なのでも立派なのでもなく、悩んで失敗して立ち止まっては迷うことを繰り返した「等身大」だからこそ伝えられることがあります。ボーク重子が伝えることは理想でもパーフェクションでもありません。それは長い迷いと諦めの時代を経てたどり着いた非認知能力を育む教育法をベースとした、自分らしい人生を自ら勝ち取るスキルなのです。<br>
+                        <br>
+                        「等身大」だからこそ「やりたい！できる！」と聴く人の心をインスパイヤし、実践的だから結果が出る。スキルの実践で「人生が変わった」のは全米最優秀女子高生コンクールで優勝した娘や自身だけじゃない。多くの人がスキルを実践することで自分らしい幸せを手に入れています。<br>
+                        <br>
+                        さあ次はあなたの番！ボーク重子と一緒に自分史上最高に幸せになりませんか？自分の可能性を最大に発揮してみませんか？一緒にそんな子育て・自分育てをしてみませんか？
+                    </p>
+                </div>
+                <h2 class="happyLife">
+                    「世界最高の子育て」を読んで問いかけを実践するようになったら子供に「ママ変わった。私の話を聞くようになってくれた」と言われて子育てに自信が持てるようになった。
+                </h2>
+                <div class="shigekoLogo">
+                    <img class="logo" src="/img/shigeko_font.png" alt="shigeko Bork">
+                    <img class="line" src="/img/line.png" alt="line">
+                </div>
+                <div class="messageMoveBack"></div>
+                <div class="messageMovie"></div>
+                <p class="messageMovieP">Message Movie</p>
+            </div>
+        </div>
+
+        <div class="mySection" id="registration">
+            <div class="mySectionInner">
+                <div class="registrationTitle">オンラインサロンのご紹介
+                    <p class="bybsOnline"> &lt;BYBSオンラインサロン&gt;</p>
+                </div>
+                <div class="content1">
+                    <div class="Content1Title">
+                        <img class="font01" src="/img/01_font.svg">
+                        <h2>
+                            <ruby>
+                                <rb><span>M</span>essage</rb>
+                                <rp>(</rp>
+                                <rt>メッセージ</rt>
+                                <rp>)</rp>
+                            </ruby>
+                        </h2>
+                    </div>
+
+                    <h3>What is <span>BYBS?</span><br>
+                        ボーク重子の<span>BYBS</span>オンラインサロンって何？</h3>
+                    <p>
+                        ライフコーチ・ボーク重子のコーチングのモットーは「パッション＋愛＋アクション＝自分らしい幸せ」。この３つをベースに自分らしく幸せな人生を構築し実践するスキルをお届けします。<br>
+                        <br>
+                        •Passion:パッションとは自分らしい幸せな人生のエンジン<br>
+                        •Love:愛とは自分を信じる力<br>
+                        •Action:アクションとは行動<br>
+                        <br>
+                        この３つを合わせた子育て・自分育てなら人生100年時代、今の子供たちは人生106年時代をどんな時も自分らしく強く幸せに生き抜くことができると思いませんか？<br>
+                        <br>
+                        コーチングメソッドに加えて20年以上にわたる起業、社長業、女性活躍、キャリア構築、非認知能力子育ての経験を踏まえた革新的なスキルを体系立てて身につけて行きます。<br>
+                        <br>
+                        2020年4月、ボーク重子のBYBS オンラインサロンスタート予定。
+                    </p>
+                </div>
+
+                <div class="content2">
+                    <div class="Content2Title">
+                        <img class="font02" src="/img/02_font.svg">
+                        <h2>
+                            <ruby>
+                                <rb><span>R</span>ecommended<br>
+                                    <span>F</span>or
+                                </rb>
+                                <rp>(</rp>
+                                <rt>こんな方にオススメ</rt>
+                                <rp>)</rp>
+                            </ruby>
+                        </h2>
+                    </div>
+
+                    <p>
+                        •著書や講演会、ワークショップでライフコーチ・ボーク重子の声にインスパイヤされた方<br>
+                        <br>
+                        •著書・講演会・ワークショップの一歩上をいく、よりパーソナルで双方向なコーチングを体験したい方<br>
+                        <br>
+                        •可能性にフタをしている自分を変えたい方<br>
+                        <br>
+                        •BYBSスキルを実践してBe Your Best Self自分史上最高の幸せを手に入れたい方<br>
+                        <br>
+                        •自分の中の非認知能力を高めたい方<br>
+                        •自分の中の非認知能力を高め、子育てに役立てたい方<br>
+                        •子供の可能性を最大に発揮せてあげたい方<br>
+                        •メンバー特典を活用してもっとパーソナルにボーク重子の世界と繋がりたい方<br>
+                        <br>
+                        •BYBSを実践する他のメンバーと繋がりたい方
+                    </p>
+                </div>
+
+                <div class="content3">
+                    <div class="Content3Title">
+                        <img class="font03" src="/img/03_font.svg">
+                        <h2>
+                            <ruby>
+                                <rb><span>S</span>pecial</rb>
+                                <rp>(</rp>
+                                <rt>特典</rt>
+                                <rp>)</rp>
+                            </ruby>
+                        </h2>
+                    </div>
+
+                    <p>
+                        •3ヶ月チャレンジ：BYBSコーチングプログラム<br>
+                        •月曜日から金曜日の朝、元気の出るラジオボークの視聴<br>
+                        •毎週１回、メンバー限定オンラインサロンライブ<br>
+                        •メンバー限定イベント・ワークショップへのご招待<br>
+                        •講演会・イベントへの優先案内<br>
+                        •質問への回答
+
+                    </p>
+                </div>
+
+
+                <div class="registrationFoot">
+                    <div class="terms">
+                        <p>規約
+                            <span>Terms of Service</span></p>
+                    </div>
+                    <div class="register">
+                        <p>加入する
+                            <span>Registration</span></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="mySection" id="media">
-        <div class="mySectionInner">
-            <div class="mediaBack">
-                <img src="img/rose.png">
-            </div>
+        <div class="mySection" id="media">
+            <div class="mySectionInner">
+                <div class="mySectionTitle">
+                    <img class="flower" src="../img/flower.svg">
+                    <h1>MEDIA</h1>
+                    <h2>これまで登場したメディア</h2>
+                </div>
+                <div class="mediaContents">
+                    <div class="newsPaper">
+                        <h3>新聞</h3>
+                        <p>日本経済新聞/産経新聞/時事通信/産経大阪/朝日新聞<br>
+                            朝日小学生新聞/北海道新聞/東京新聞/中日新聞<br>
+                            読売新聞/山口新聞/北羽新聞/福島民報新聞/福島民友新聞<br>
+                            中国新聞
+                        </p>
+                    </div>
+                    <div class="online">
+                        <h3>オンライン</h3>
+                        <p>Yahooニュース/産経ニュース/東洋経済オンライン<br>
+                            ダイヤモンドオンライン/ホウドウキョク/日経Dual<br>
+                            News picks/ダヴィンチ・ニュース/Schoo/リセマム<br>
+                            Mammemo/日刊スゴイ人/子ども学びラボ/Eduナビ<br>
+                            ママスタ/インターエヂュ/マイナビ進路のミカタ<br>
+                            マイナビ保育士/Woman Excite/本Tube/マナビコ<br>
+                            ママそら/たまひよ/Kidsna
+                        </p>
+                    </div>
+                    <div class="magazine">
+                        <h3>雑誌</h3>
+                        <p>文藝春秋/プレジデントファミリー/日経ウーマン<br>
+                            女性セブン/Very/Chanto/小学一年生<br>
+                            月間「海外子女教育」/ディズニーツムツム/伸芽会会報<br>
+                            しゃくなげ会報/リビング福島/Lau<br>
+                            代々木ゼミナール会報/NEC労働組合
+                        </p>
+                    </div>
+                    <div class="tvRadio">
+                        <h3>テレビ・ラジオ</h3>
+                        <p>人生が変わる1分間の深イイ話/踊る！さんま御殿！！<br>
+                            フジTV:ノンストップ/TBS：親の顔見たい<br>
+                            福島中央：ゴジテレ/J-wave:Step one/Zip FM<br>
+                            ラジオ大阪/岐阜市みんなのラジオ
+                        </p>
+                    </div>
 
-            <div class="mediaTitle">
-                <h2>Media</h2>
-            </div>
-            <div class="mediaContents">
-                <p>ああああああああああああああああ/あああああああああああああああああああああああああああ/ああああああ/ああああああああああああああ/ああああああああああああああああああああああああ/あああああああああ
-                </p>
-            </div>
-            <div class="mediaButton">
-                <a href="..."><p>More >></p></a>
+                    <img class="rose" src="img/rose.png">
+
+                </div>
             </div>
         </div>
-    </div>
-    <div class="mySection" id="news">
-        <div class="mySectionInner">
-            <div class="newsBack1"></div>
-            <div class="newsBack2"></div>
-            <div class="newsTitle">
-                <h2>News</h2>
-            </div>
-            <div class="newsContents">
-                <h3>最新記事：あああああああああああ</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="newsMore">
-                <a href="..."><p>続きを読む</p></a>
+        <div class="mySection" id="press">
+            <div class="mySectionInner">
+                <div class="mySectionTitle">
+                    <img class="flower" src="../img/flower.svg">
+                    <h1>PRESS</h1>
+                    <h2>プレス</h2>
+                </div>
+
+                <div class="contentNews">
+                    <div class="newsTitle">
+                        <h2>News</h2>
+                    </div>
+                    <div class="newsContents">
+                        <h3>最新記事：イベント情報を更新しました</h3>
+                        <p>あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
+                    </div>
+                    <div class="newsMore">
+                        <a href="..."><p>続きを読む</p></a>
+                    </div>
+                </div>
+
+                <div class="contentContact">
+                    <div class="contactTitle">
+                        <h2>Contact</h2>
+                    </div>
+                    <div class="contactButton">
+
+                        <a href="..."><p>取材依頼：<br>
+                                shigeko@shigekobork.com<br>
+                                にお願いします！</p></a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="mySection" id="contact">
-        <div class="mySectionInner">
-            <div class="contactBack"></div>
-            <div class="contactTitle">
-                <h2>Contact</h2>
-            </div>
-            <div class="contactButton">
-                <div class="contactButtonBack"></div>
-                <a href="..."><p>メールでお問い合わせ<br>
-                        取材はこちらまで</p></a>
-            </div>
-        </div>
-    </div>
+
 
 <?php getFooter(); ?>
